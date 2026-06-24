4c647931b2130bffd8855d03deb5cb5f60bd98bf
hakanyalniz <hakan_yalniz@live.com>
2026-06-24 16:39:49 +0300

    The user can now update the localStorage object with a button click.

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,60 @@
 import "./App.css";
+import { useState } from "react";
+
+// temporary types
+type Skills = {
+  [key: string]: number;
+};
+
+type UserStatus = {
+  name: string;
+  level: number;
+  skills: Skills;
+};
+
+const defaultStatus: UserStatus = {
+  name: "Test",
+  level: 0,
+  skills: {},
+};
 
 function App() {
   const storageKey = "test_status";
 
-  const userStatus = {
-    name: "Test",
-    level: 0,
-    skills: {
-      meditating: 0,
-    },
+  const [userState, setUserState] = useState<UserStatus>(() => {
+    const savedData = localStorage.getItem(storageKey);
+    return savedData ? JSON.parse(savedData) : defaultStatus;
+  });
+
+  const increaseProfiency = (skillName: string, skillLevel: number) => {
+    const savedDataRaw = localStorage.getItem(storageKey);
+    const currentStatus = savedDataRaw ? JSON.parse(savedDataRaw) : {};
+
+    const updatedProfile: UserStatus = {
+      ...currentStatus,
+      skills: { ...currentStatus.skills, [skillName]: skillLevel + 1 },
+    };
+    setUserState(updatedProfile);
+    localStorage.setItem(storageKey, JSON.stringify(updatedProfile));
   };
-  localStorage.setItem(storageKey, JSON.stringify(userStatus));
-
-  const savedDataRaw = localStorage.getItem(storageKey);
-  const currentStatus = savedDataRaw ? JSON.parse(savedDataRaw) : {};
-
-  const updatedProfile = {
-    ...currentStatus,
-    skills: { ...currentStatus.skills, exercise: 0 },
-  };
-  localStorage.setItem(storageKey, JSON.stringify(updatedProfile));
-
-  console.log(updatedProfile);
 
   return (
     <>
       <div>
         <p>Status</p>
-        <p>{updatedProfile.name}</p>
-        <p>{updatedProfile.level}</p>
+        <p>{userState.name}</p>
+        <p>{userState.level}</p>
         <p>Skills: </p>
         <ul>
           {/* Object.entries turns the skills object into an array of [key, value] pairs */}
-          {Object.entries(updatedProfile.skills).map(
-            ([skillName, skillLevel]) => (
-              <li key={skillName}>
-                <strong>{skillName}:</strong> {skillLevel}
-              </li>
-            ),
-          )}
+          {Object.entries(userState.skills).map(([skillName, skillLevel]) => (
+            <li key={skillName}>
+              <strong>{skillName}:</strong> {skillLevel}
+              <button onClick={() => increaseProfiency(skillName, skillLevel)}>
+                Increase
+              </button>
+            </li>
+          ))}
         </ul>
       </div>
     </>
