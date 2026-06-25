9caeb4a91299f172aadd005faa6f56655e230a58
hakanyalniz <hakan_yalniz@live.com>
2026-06-25 17:39:18 +0300

    Cleaned things up and added comments.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -20,15 +20,19 @@ function Home() {
   // State for the input field for skill
   const [inputText, setInputText] = useState("");
 
-  /** Clicking increase button next to the skill increases the local state and the local storage data. */
+  /** Clicking increase button next to the skill increases the local state and the local storage data.
+   * It also updates the total user level, which is a sum of all skill experience.*/
   const increaseProficiency = (skillName: string, skillExperience: number) => {
+    // Update the total skill experience of the specific skill
     const newSkillExperience = skillExperience + 10;
 
+    // Update the specific skill experience
     const updatedSkills = {
       ...userState.skills,
       [skillName]: newSkillExperience,
     };
 
+    // Get the sum of all skill experience, turn it into level, which is the general user level
     const totalUserLevel = Math.round(
       Object.values(updatedSkills).reduce((sum, value) => {
         const modifiedValue = Math.round(handleExperienceImplementation(value));
@@ -36,6 +40,7 @@ function Home() {
       }, 0),
     );
 
+    // Get it all together
     const updatedProfile = {
       ...userState,
       level: totalUserLevel,
@@ -65,7 +70,8 @@ function Home() {
     localStorage.setItem(storageKey, JSON.stringify(updatedState));
   };
 
-  /** Uses a math formula to determine skill levels given skill experience. Every 10 experience, increased by 10 percent each level, is 1 level. */
+  /** Uses a math formula to determine skill levels given skill experience. Every 10 experience, increased by 10 percent each level, is 1 level.
+   * Numbers are logged as experience in skill proficiency, it is while rendering that they are converted to levels.*/
   const handleExperienceImplementation = (skillExperience: number) => {
     const argument = (0.1 * skillExperience) / 10 + 1;
     const skillLevel = Math.log(argument) / Math.log(1.1);
